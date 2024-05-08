0efbb74ab29144f8166b933488f4c882f9296a77
Kwan <nhimcoi262@gmail.com>
2024-05-08 11:07:05 +0700

    Completed HW1 functions

--- HW1/ex1.py
+++ HW1/ex1.py
@@ -4,68 +4,54 @@ import matplotlib.pyplot as plt
 
 # Load an image from file as function
 def load_image(image_path):
-    """
-    Load an image from file, using OpenCV
-    """
-    pass
+    image = cv2.imread(image_path)
+    return image
 
 # Display an image as function
 def display_image(image, title="Image"):
-    """
-    Display an image using matplotlib. Rembember to use plt.show() to display the image
-    """
-    pass
-
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(title)
+    plt.axis("off")
 
 # grayscale an image as function
 def grayscale_image(image):
-    """
-    Convert an image to grayscale. Convert the original image to a grayscale image. In a grayscale image, the pixel value of the
-    3 channels will be the same for a particular X, Y coordinate. The equation for the pixel value
-    [1] is given by:
-        p = 0.299R + 0.587G + 0.114B
-    Where the R, G, B are the values for each of the corresponding channels. We will do this by
-    creating an array called img_gray with the same shape as img
-    """
-    pass
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return img_gray
 
 
 # Save an image as function
 def save_image(image, output_path):
-    """
-    Save an image to file using OpenCV
-    """
-    pass
+    cv2.imwrite(output_path, image)
 
 
 # flip an image as function 
 def flip_image(image):
-    """
-    Flip an image horizontally using OpenCV
-    """
-    pass
+    flipped_image = cv2.flip(image, 1)  # 1: Horizontal flip
+    return flipped_image
 
 
 # rotate an image as function
 def rotate_image(image, angle):
-    """
-    Rotate an image using OpenCV. The angle is in degrees
-    """
-    pass
+    height, width = image.shape[:2]
+    rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
+    rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
+    return rotated_image
 
 
 if __name__ == "__main__":
     # Load an image from file
-    img = load_image("images/uet.png")
+    img = load_image("uet.png")
 
     # Display the image
     display_image(img, "Original Image")
-
+    plt.show()
+    
     # Convert the image to grayscale
     img_gray = grayscale_image(img)
 
     # Display the grayscale image
     display_image(img_gray, "Grayscale Image")
+    plt.show()
 
     # Save the grayscale image
     save_image(img_gray, "images/lena_gray.jpg")
@@ -75,15 +61,20 @@ if __name__ == "__main__":
 
     # Display the flipped grayscale image
     display_image(img_gray_flipped, "Flipped Grayscale Image")
+    plt.show()
 
     # Rotate the grayscale image
-    img_gray_rotated = rotate_image(img_gray, 45)
+    img_gray_rotated = rotate_image(img_gray, 70)
 
     # Display the rotated grayscale image
     display_image(img_gray_rotated, "Rotated Grayscale Image")
+    plt.show()
 
     # Save the rotated grayscale image
     save_image(img_gray_rotated, "images/lena_gray_rotated.jpg")
 
-    # Show the images
-    plt.show() 
+    img_flipped = flip_image(img)
+    img_rotated = rotate_image(img, 70)
+    save_image(img_flipped, "./uet_flipped.jpg")
+    save_image(img_rotated, "./uet_rotated.jpg")
+    
